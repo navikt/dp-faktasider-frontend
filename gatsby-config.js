cee2f1a73d79c338dadc793e94f493c2f3b35c37
Daniel Winsvold <danielwinsvold@gmail.com>
2020-05-06 15:15:17 +0200

    legger til nav-decorator vha plugin fra Sykdom i familien https://github.com/navikt/sykdom-i-familien/tree/master/plugins/nav-decorator

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-typescript',
+    'nav-decorator',
     {
       resolve: 'gatsby-source-sanity',
       options: {
---
+++ plugins/nav-decorator/getDecorator.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const requestDecorator = (callback) => {
+  const url =
+    process.env.NAIS_CLUSTER_NAME === 'dev-sbs'
+      ? 'https://www-q0.nav.no/person/nav-dekoratoren/'
+      : 'https://appres.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true';
+  return request(url, callback);
+};
+
+const getDecorator = () =>
+  new Promise((resolve, reject) => {
+    const callback = (error, response, body) => {
+      if (!error && response.statusCode >= 200 && response.statusCode < 400) {
+        const { document } = new JSDOM(body).window;
+        const data = {
+          NAV_SCRIPTS: document.getElementById('scripts').innerHTML,
+          NAV_STYLES: document.getElementById('styles').innerHTML,
+          NAV_HEADING: document.getElementById('header-withmenu').innerHTML,
+          NAV_FOOTER: document.getElementById('footer-withmenu').innerHTML,
+          NAV_MENU_RESOURCES: document.getElementById('megamenu-resources').innerHTML,
+        };
+
+        resolve(data);
+      } else {
+        reject(new Error(error));
+      }
+    };
+    requestDecorator(callback);
+  });
+
+module.exports = getDecorator;
