92d45fcdd01ada13891e060a2ed82945cfcf32d0
kriise <kriise@nav>
2020-05-06 10:06:46 +0200

    greier å hente ut data fra sanity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,38 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export const query = graphql`
+  query Titles {
+    titles: allSanityFaktaSide {
+      edges {
+        node {
+          title {
+            nb
+          }
+        }
+      }
+    }
+  }
+`;
 
-export default IndexPage
+const IndexPage = (props) => {
+  var titles = props.data.titles.edges.map((edge) => edge.node.title.nb);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {titles.map((title) => (
+        <h2 key={title}>{title}</h2>
+      ))}
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
+
+export default IndexPage;
