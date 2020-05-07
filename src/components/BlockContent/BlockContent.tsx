9038a59fbfc5027bdbc555f316498eefc46998a5
Daniel Winsvold <danielwinsvold@gmail.com>
2020-05-07 08:55:34 +0200

    litt typing av serializers for å hjelpe til når man skal lage nye

--- src/components/BlockContent/BlockContent.tsx
+++ src/components/BlockContent/BlockContent.tsx
@@ -2,12 +2,19 @@ import * as React from 'react';
 import SanityBlockContent from '@sanity/block-content-to-react';
 import CustomComponent from './CustomComponent';
 import { GtoNOK } from '../../utils/folketrygdensGrunnbeløp';
+import { ReactNode } from 'react';
 
 interface Props {
   blocks: any;
 }
 
-const serializers = {
+type Serializers = {
+  types: {
+    [key: string]: ({ node: any }) => ReactNode;
+  };
+};
+
+const serializers: Serializers = {
   types: {
     localeRichText: (props) => <BlockContent blocks={props.node.nb} />,
     customComponent: CustomComponent,
