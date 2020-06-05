90e66cafc389f68ddbd8648a672613097e07ba18
Daniel Winsvold <danielwinsvold@gmail.com>
2020-06-05 15:05:58 +0200

    tar høyde for ny dekoratør og pop-down ved klikk i innholdsmeny som fører til scroll oppover. Tittel ble før skjult bak pop-downmeny, men tar nå høyde for denne

--- src/styles/GlobalStyling.tsx
+++ src/styles/GlobalStyling.tsx
@@ -1,10 +1,10 @@
+import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import { theme } from './theme';
 import './index.less';
+import { useDekoratorPopdownOffset } from '../templates/faktaside/InnholdsMeny/useDekoratorPopdownOffset';
 
-const paddingTop = '2rem';
-
-export const GlobalStyling = createGlobalStyle`
+const Styling = createGlobalStyle<{ dekoratorOffset: number }>`
     * {
         box-sizing: border-box;
     }
@@ -18,11 +18,12 @@ export const GlobalStyling = createGlobalStyle`
 
     // :target = valgt i url (url-fragment), sørger for at header ikke ligger klint oppi toppen ved bruk av interne url'er https://css-tricks.com/hash-tag-links-padding/
     :target, .internUrl {
+      --offset-top: calc(2rem + ${(props) => props.dekoratorOffset}px);
         &::before {
             content: "";
             display: block;
-            height: ${paddingTop};
-            margin-top: -${paddingTop};
+            height: var(--offset-top);
+            margin-top: -var(--offset-top);
         }
         position: relative;
         &:focus {
@@ -34,7 +35,7 @@ export const GlobalStyling = createGlobalStyle`
               bottom: 0;
               left: 0;
               width: 100%;
-              height: calc(100% - ${paddingTop});
+              height: calc(100% - var(--offset-top));
               ${theme.focus};
               border-radius: .2rem;
             }
@@ -45,6 +46,11 @@ export const GlobalStyling = createGlobalStyle`
         transition: height 0.3s;
     }
 `;
+
+export function GlobalStyling() {
+  const offset = useDekoratorPopdownOffset();
+  return <Styling dekoratorOffset={offset} />;
+}
 
 /* Scoper denne stylingen så den ikke overskriver styling i Meny/Dekorator */
 export const AppStyling = styled.div`
