a32ffdbbe5ec590c9124bdf022c938151a0aa42e
Daniel Winsvold <danielwinsvold@gmail.com>
2020-06-19 13:26:46 +0200

    skriver om parser til å være mer skalerbar slik at den er enklere å utvide med funksjonalitet, bla håndtere at grupper skal kunne vises som utkast, merkes med vis-for etc

---
+++ src/components/BlockContent/GroupMarkup.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Group } from '../../utils/richTextUtils/richTextTypes';
+import Utkast from './Utkast';
+import H2GroupMarkup from './H2GroupMarkup';
+import H3GroupMarkup from './H3GroupMarkup';
+import withErrorBoundary from '../withErrorBoundary';
+import H4GroupMarkup from './H4GroupMarkup';
+
+interface Props {
+  node: Group;
+}
+
+function getContent(group: Group) {
+  switch (group.groupType) {
+    case 'h2':
+      return <H2GroupMarkup {...group} />;
+    case 'h3':
+      return <H3GroupMarkup {...group} />;
+    case 'h4':
+      return <H4GroupMarkup {...group} />;
+    default:
+      throw Error(`Ukjent gruppe: ${group.groupType}`);
+  }
+}
+
+function GroupMarkup(props: Props) {
+  if (props.node?.erUtkast) {
+    return <Utkast>{getContent(props.node)}</Utkast>;
+  }
+
+  return getContent(props.node);
+}
+
+export default withErrorBoundary(GroupMarkup, 'GroupMarkup');
