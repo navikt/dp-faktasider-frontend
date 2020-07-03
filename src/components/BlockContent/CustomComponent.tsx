ad50a0b836f337ce9138c8027539ed79f93af2ff
Daniel Winsvold <danielwinsvold@gmail.com>
2020-07-03 14:16:11 +0200

    legger til dagpengekalkulator for lærlinger

--- src/components/BlockContent/CustomComponent.tsx
+++ src/components/BlockContent/CustomComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 import DagpengerKalkulator from '../HvorMyeKalkulator/DagpengerKalkulator';
+import DagpengerKalkulatorLRling from '../HvorMyeKalkulator/DagpengerKalkulatorLærling';
 
 interface Props {
   node: {
@@ -12,6 +13,8 @@ function CustomComponent(props: Props) {
   switch (props.node.komponent) {
     case 'DagpengeKalkulator - normal':
       return <DagpengerKalkulator />;
+    case 'DagpengeKalkulator - lærling':
+      return <DagpengerKalkulatorLRling />;
     default:
       return <AlertStripeFeil>Ukjent komponent: "{props.node.komponent}"</AlertStripeFeil>;
   }
