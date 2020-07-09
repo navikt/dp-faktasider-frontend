34a094bb9a616cb59beb9415936b918c40848ddd
kriise <knut.magne.riise@nav.no>
2020-07-09 10:22:22 +0200

    flyttet ut produksjonssjekken i logger

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,7 +1,8 @@
 import amplitude from 'amplitude-js';
+import { isProduction } from './environment';
 
 const getApiKey = () => {
-  if (window.location.hostname.includes('www.nav.no')) {
+  if (isProduction()) {
     return 'edf391bf01b758a289ef5e7cb297f77a'; // prod
   }
   return '24eb6d83cfc9883c04c4eaec61251bf4'; // dev
@@ -16,14 +17,12 @@ logging.init(getApiKey(), '', {
   includeReferrer: true,
 });
 
-const loggEvent = (event: string, ekstraData?: object) => {
-  logging.logEvent(event, { ...ekstraData, appName: 'dp-faktasider' });
-};
+const loggEvent = (event: string, ekstraData?: object) =>
+  logging.logEvent(event, {
+    ...ekstraData,
+    appName: 'dp-faktasider',
+  });
 
-export const loggError = (msg: string, ekstraData?: object) => {
-  loggEvent('Error', { ...ekstraData, msg });
-};
+export const loggError = (msg: string, ekstraData?: object) => loggEvent('Error', { ...ekstraData, msg });
 
-export const loggSidevisning = (side: string, ekstraData?: object) => {
-  loggEvent('Side vist', { ...ekstraData, side });
-};
+export const loggSidevisning = (side: string, ekstraData?: object) => loggEvent('Side vist', { ...ekstraData, side });
