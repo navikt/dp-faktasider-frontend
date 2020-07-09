11b9666b0861f7d4d4ed2c7260a51c1a15715a41
kriise <knut.magne.riise@nav.no>
2020-07-09 10:18:23 +0200

    lagt på amplitude, samt to logging funksjoner, logger nå hvis errorboundary treffes og for hver gang en faktaside lages.

---
+++ src/utils/logging.ts
@@ -0,0 +1,29 @@
+import amplitude from 'amplitude-js';
+
+const getApiKey = () => {
+  if (window.location.hostname.includes('www.nav.no')) {
+    return 'edf391bf01b758a289ef5e7cb297f77a'; // prod
+  }
+  return '24eb6d83cfc9883c04c4eaec61251bf4'; // dev
+};
+
+const logging = amplitude.getInstance();
+logging.init(getApiKey(), '', {
+  apiEndpoint: 'amplitude.nav.no/collect',
+  saveEvents: false,
+  includeUtm: true,
+  batchEvents: false,
+  includeReferrer: true,
+});
+
+const loggEvent = (event: string, ekstraData?: object) => {
+  logging.logEvent(event, { ...ekstraData, appName: 'dp-faktasider' });
+};
+
+export const loggError = (msg: string, ekstraData?: object) => {
+  loggEvent('Error', { ...ekstraData, msg });
+};
+
+export const loggSidevisning = (side: string, ekstraData?: object) => {
+  loggEvent('Side vist', { ...ekstraData, side });
+};
