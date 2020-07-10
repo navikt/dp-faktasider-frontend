ccc8554ecc35065089494d1efd105cab3810ef90
Daniel Winsvold <danielwinsvold@gmail.com>
2020-07-10 16:19:21 +0200

    for å skille mellom q-miljø og nav.no må vi sjekke på location, environment er production for begge

--- src/utils/loggingConfig.ts
+++ src/utils/loggingConfig.ts
@@ -1,8 +1,7 @@
 import amplitude from 'amplitude-js';
-import { isProduction } from './environment';
 
 const getApiKey = () => {
-  if (isProduction()) {
+  if (window.location.href.includes('www.nav.no')) {
     return 'edf391bf01b758a289ef5e7cb297f77a'; // prod
   }
   return '24eb6d83cfc9883c04c4eaec61251bf4'; // dev
