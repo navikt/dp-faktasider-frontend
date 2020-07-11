e619aa3cb63053a8488f970b2b36cd38e5d55bbe
Daniel Winsvold <danielwinsvold@gmail.com>
2020-07-11 09:37:41 +0200

    litt ryddigere mocking av useFaktasiderSumary

--- src/templates/faktaside/FaktaSide.test.tsx
+++ src/templates/faktaside/FaktaSide.test.tsx
@@ -3,12 +3,10 @@ import { render } from '../../utils/test-utils';
 import FaktaSide from './FaktaSide';
 import { createFaktasideContext } from '../../../gatsby-utils/createFaktasider';
 import { rawFaktasideDataMock } from '../../../__mocks__/rawFaktasideDataMock';
-import { faktasiderSummaryMockData } from '../../utils/faktasiderSummary/faktasiderSummaryMockData';
-import useFaktasiderSumary from '../../utils/faktasiderSummary/useFaktasiderSumary';
 
-jest.mock('../../utils/faktasiderSummary/useFaktasiderSumary');
-// @ts-ignore
-useFaktasiderSumary.mockReturnValue(faktasiderSummaryMockData);
+jest.mock('../../utils/faktasiderSummary/useFaktasiderSumary', () => () => {
+  return require('../../utils/faktasiderSummary/faktasiderSummaryMockData').faktasiderSummaryMockData;
+});
 
 /* Dette er en veldig grov test som fanger opp det meste av endringer på faktaside */
 test('Snapshottest Faktaside', () => {
