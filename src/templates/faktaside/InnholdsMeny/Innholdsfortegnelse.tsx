fecc220bc14b36dea7bb442da7b5f4ee137b1af1
Daniel Winsvold <danielwinsvold@gmail.com>
2020-07-16 10:46:54 +0200

    mulighet til å lukke innholdsmeny, og litt styling med chevrons i meny

--- src/templates/faktaside/InnholdsMeny/Innholdsfortegnelse.tsx
+++ src/templates/faktaside/InnholdsMeny/Innholdsfortegnelse.tsx
@@ -62,7 +62,8 @@ function MenuItem(props: { item: Group; current: boolean }) {
 }
 
 const StyledOl = styled.ol`
-  margin: 0.5rem 1rem 1rem 2.5rem;
+  padding: 0.5rem 1rem 1rem 2.5rem;
+  border-right: 0.3rem solid #8884;
 `;
 
 function Innholdsfortegnelse() {
--- src/templates/faktaside/Navigasjonsmeny/SideListe.tsx
+++ src/templates/faktaside/Navigasjonsmeny/SideListe.tsx
@@ -1,44 +1,96 @@
 import * as React from 'react';
+import { useReducer } from 'react';
 import { Link } from 'gatsby';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import withErrorBoundary from '../../../components/withErrorBoundary';
-import useFaktasiderSumary from '../../../utils/faktasiderSummary/useFaktasiderSumary';
+import useFaktasiderSumary, { FaktasideSummary } from '../../../utils/faktasiderSummary/useFaktasiderSumary';
 import Innholdsfortegnelse from '../InnholdsMeny/Innholdsfortegnelse';
 import { useFaktasideContext } from '../FaktaSideContext';
 import { loggMeny } from '../../../utils/logging';
+import NavFrontendChevron from 'nav-frontend-chevron';
+import { UnmountClosed } from 'react-collapse';
 
 const StyledOl = styled.ol`
   margin-bottom: 3.5rem;
 `;
 
-const StyledLink = styled(Link)`
-  display: block;
+const listeElementCommonStyling = css`
+  display: flex;
   font-size: 1.1rem;
   font-weight: 600;
   padding: 0.5rem 1.5rem;
   text-decoration: none;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+  > *:last-child {
+    transition: 0.3s;
+    opacity: 0.6;
+  }
   &:hover {
     background-color: #ddd8;
+    > *:last-child {
+      margin-right: -0.5rem;
+    }
   }
+  ${listeElementCommonStyling};
 `;
+
+const StyledButton = styled.button`
+  ${listeElementCommonStyling};
+  width: 100%;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  text-align: start;
+`;
+
+function SideListeElement(props: { page: FaktasideSummary }) {
+  const faktasideContext = useFaktasideContext();
+  const [open, toggle] = useReducer((state) => !state, true);
+
+  const currentPage = props.page.id === faktasideContext.id;
+
+  if (currentPage) {
+    return (
+      <>
+        <StyledButton
+          onClick={() => {
+            toggle();
+            loggMeny('Åpne/lukke innholdsfortegnelse');
+          }}
+          aria-expanded={open}
+        >
+          <span>{props.page.tittel}</span>
+          <NavFrontendChevron type={open ? 'opp' : 'ned'} />
+        </StyledButton>
+        <UnmountClosed isOpened={open}>
+          <Innholdsfortegnelse />
+        </UnmountClosed>
+      </>
+    );
+  }
+
+  return (
+    <StyledLink className="lenke" to={props.page.path} onClick={() => loggMeny('Gå til ny side')}>
+      <span>
+        {props.page.tittel} {!props.page.tilgjengeligPåValgtSpråk ? `(${props.page.språk})` : ''}
+      </span>
+      <NavFrontendChevron type="høyre" />
+    </StyledLink>
+  );
+}
 
 function SideListe() {
   const otherPages = useFaktasiderSumary();
-  const faktasideContext = useFaktasideContext();
 
   return (
     <StyledOl>
-      {otherPages.map((page) => (
-        <li key={page.id}>
-          <StyledLink
-            activeStyle={{ color: 'black' }}
-            className="lenke"
-            to={page.path}
-            onClick={() => loggMeny('Gå til ny side')}
-          >
-            {page.tittel} {!page.tilgjengeligPåValgtSpråk ? `(${page.språk})` : ''}
-          </StyledLink>
-          {page.id === faktasideContext.id && <Innholdsfortegnelse />}
+      {otherPages.map((side) => (
+        <li key={side.id}>
+          <SideListeElement page={side} />
         </li>
       ))}
     </StyledOl>
