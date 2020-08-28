60182f959f63362c79451e9f57f89641b42ccb49
Daniel Winsvold <danielwinsvold@gmail.com>
2020-08-28 09:50:21 +0200

    møter kontrastkrav

--- src/components/BlockContent/Tilleggsinnformasjon/TilleggsInnformasjon.tsx
+++ src/components/BlockContent/Tilleggsinnformasjon/TilleggsInnformasjon.tsx
@@ -36,7 +36,7 @@ const StyledAside = styled.aside<{ open: boolean }>`
 `;
 
 const Label = styled(Undertekst)`
-  opacity: 0.5;
+  opacity: 0.45;
   font-size: 0.7rem;
   margin: 0 !important;
   text-transform: uppercase;
