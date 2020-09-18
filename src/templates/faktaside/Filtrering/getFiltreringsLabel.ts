ff0d360d2fdeacdcd46315ace157a7758d731ab7
Daniel Winsvold <danielwinsvold@gmail.com>
2020-09-18 12:30:40 +0200

    legger til labels for nye filtreringsalternativer

--- src/templates/faktaside/Filtrering/getFiltreringsLabel.ts
+++ src/templates/faktaside/Filtrering/getFiltreringsLabel.ts
@@ -2,6 +2,10 @@ const labelMap = {
   SelvstendigNaeringsdrivende: 'Selvstendig næringsdrivende',
   Laerling: 'Lærling',
   Konkurs: 'Arbeidsgiver er konkurs',
+  SagtOppSelv: 'Sier opp selv',
+  MottarAnnenStotteFraNav: 'Mottar annen økonomisk støtte fra NAV',
+  StreikLockout: 'Streik eller lockout',
+  MottarVentelonn: 'Mottar ventelønn',
 };
 
 export function getFiltreringsvalgLabel(valg: string) {
