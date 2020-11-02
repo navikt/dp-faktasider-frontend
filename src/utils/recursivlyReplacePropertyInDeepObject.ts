cbe30e74cae4b1372906dbd3ee61fe830e22eefa
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-02 12:43:01 +0100

    refaktorering, trekker ut i generisk funksjon for at det skal bli lettere å lese intensjon i koden

---
+++ src/utils/recursivlyReplacePropertyInDeepObject.ts
@@ -0,0 +1,21 @@
+export function recursivlyReplacePropertyInDeepObject(
+  obj,
+  propertyToReplace: string,
+  replaceFunction: (current) => any
+) {
+  if (Array.isArray(obj)) {
+    return obj.map((v) => recursivlyReplacePropertyInDeepObject(v, propertyToReplace, replaceFunction));
+  } else if (obj && typeof obj == 'object') {
+    return Object.keys(obj).reduce((acc, key) => {
+      return {
+        ...acc,
+        [key]:
+          key === propertyToReplace
+            ? replaceFunction(obj[key])
+            : recursivlyReplacePropertyInDeepObject(obj[key], propertyToReplace, replaceFunction),
+      };
+    }, {});
+  }
+
+  return obj;
+}
