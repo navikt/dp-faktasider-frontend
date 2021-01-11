a846b34b9de58c2fdd6d6aa1eeb240ddf9d22ecf
Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
2021-01-11 15:38:15 +0100

    Start migrering

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withLess = require("@zeit/next-less");
+
+const navFrontendModuler = Object.keys(
+  require("./package.json").dependencies
+).filter((it) => it.startsWith("nav-"));
+
+const withTranspileModules = require("next-transpile-modules")(
+  navFrontendModuler
+);
+
+module.exports = withTranspileModules(
+  withLess()
+);
