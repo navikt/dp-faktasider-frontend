14cc4d204a818cd0e38418e2072bc5dbbc62606e
Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
2021-01-15 13:38:30 +0100

    fix liveliness routes

--- src/pages/api/isready.js
+++ src/pages/api/isready.js
@@ -1,12 +1,4 @@
-import { createSessionDb } from "../../lib/userDb";
-
-const session = createSessionDb();
-
 export default async (req, res) => {
-  if (!(await session.isReady())) {
-    res.statusCode = 503;
-  } else {
-    res.statusCode = 200;
-  }
+  res.statusCode = 200;
   res.end();
-};
+}
