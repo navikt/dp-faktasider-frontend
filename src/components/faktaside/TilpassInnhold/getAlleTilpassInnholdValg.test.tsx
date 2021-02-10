3378ce2da8250d10f39ba13daf7bdfb633cc7da6
Daniel Winsvold <danielwinsvold@gmail.com>
2021-02-10 09:35:51 +0100

    fikser bug med markDef som kan eksistere på sanity-data selv om den ikke er i bruk.
    Tror dette oppstod ved copy-pasting av tekst i sanity. Sanity prøver nok å ivareta marks, men her skjedde det noe rart som gjorde at en markDef ble tatt vare på selv om den ikke var i bruk. Dette førte til at en visFor-markDef ble ivaretatt selv om den ikke var i bruk, som gjorde at denne situasjonen dukka opp i filteret. Må derfor sjekke at markDefs faktisk er i bruk før de vises i filteret.

--- src/components/faktaside/TilpassInnhold/getAlleTilpassInnholdValg.test.tsx
+++ src/components/faktaside/TilpassInnhold/getAlleTilpassInnholdValg.test.tsx
@@ -1,30 +1,67 @@
 import parseRichText from "../../../utils/richTextUtils/parser/parseRichText";
 import getAlleTilpassInnholdValg from "./getAlleTilpassInnholdValg";
-import { SanityBlock } from "../../../utils/richTextUtils/richTextTypes";
+import { Block, SanityBlock } from "../../../utils/richTextUtils/richTextTypes";
 import { createSanityBlock } from "../../../testUtils/createSanityBlock";
 
-const getAlleTilpassInnholdValgTestdata: SanityBlock[] = [
-  createSanityBlock("Skal vises for student", "normal", { visFor: ["student"] }),
-  createSanityBlock("Skal også vises for student", "normal", { visFor: ["student"] }),
-  createSanityBlock("Overskrift", "h2"),
-  createSanityBlock("Jeg har en mark som ikke skal regnes med", "normal", { marks: ["randomMark"] }),
-  {
-    ...createSanityBlock("Jeg har en markDef som ikke skal regnes med", "normal"),
-    markDefs: [
-      {
-        _type: "notRelevant",
-        _key: "N/A",
-      },
-    ],
-  },
-  createSanityBlock("Skal vises for permittert", "normal", { visFor: ["permittert"] }),
-];
+describe("getAlleFiltreringsValgForInnhold", () => {
+  test("lager en liste over alle tilgjengelige filtrerinsvalg for innhold", () => {
+    const getAlleTilpassInnholdValgTestdata: SanityBlock[] = [
+      createSanityBlock("Skal vises for student", { visFor: ["student"] }),
+      createSanityBlock("Skal også vises for student", { visFor: ["student"] }),
+      createSanityBlock("Overskrift", { style: "h2" }),
+      createSanityBlock("Jeg har en mark som ikke skal regnes med", { marks: ["randomMark"] }),
+      createSanityBlock("Skal vises for permittert", { visFor: ["permittert"] }),
+    ];
 
-test("getAlleFiltreringsValgForInnhold lager en liste over alle tilgjengelige filtrerinsvalg for innhold", () => {
-  const parsedText = parseRichText(getAlleTilpassInnholdValgTestdata);
-  const filtreringsValg = getAlleTilpassInnholdValg(parsedText);
+    const parsedText = parseRichText(getAlleTilpassInnholdValgTestdata);
+    const filtreringsValg = getAlleTilpassInnholdValg(parsedText);
 
-  expect(filtreringsValg).toHaveLength(2);
-  expect(filtreringsValg).toContain("student");
-  expect(filtreringsValg).toContain("permittert");
+    expect(filtreringsValg).toHaveLength(2);
+    expect(filtreringsValg).toContain("student");
+    expect(filtreringsValg).toContain("permittert");
+  });
+
+  test("tar med visFor-info på gruppe-config", () => {
+    const getAlleTilpassInnholdValgTestdata: SanityBlock[] = [
+      createSanityBlock("Overskrift", { style: "h2", visFor: ["student"] }),
+      createSanityBlock("Skal vises for permittert"),
+    ];
+
+    const parsedText = parseRichText(getAlleTilpassInnholdValgTestdata);
+    const filtreringsValg = getAlleTilpassInnholdValg(parsedText);
+
+    expect(filtreringsValg).toHaveLength(1);
+    expect(filtreringsValg).toContain("student");
+  });
+
+  test("tar ikke med markDefs som ikke er i bruk", () => {
+    const blockMedUbrukMarkDef: Block = {
+      _key: "04180ae4e0fc",
+      _type: "block",
+      children: [
+        {
+          _key: "6744644ccab5",
+          _type: "span",
+          marks: [], // _key-verdien fra markdef må ligge her for at den skal bli brukt, det har hendt at det dukker opp ubrukte markDefs i sanity.
+          text: "Har du rett til dagpenger når du mottar annen økonomisk støtte fra NAV?",
+        },
+      ],
+      markDefs: [
+        {
+          _key: "487ac3d50a15",
+          _type: "visForAnnotation",
+          visFor: {
+            _type: "visFor",
+            situasjoner: ["Mottar annen støtte fra NAV"],
+          },
+        },
+      ],
+      style: "h2",
+    };
+
+    const parsedText = parseRichText([blockMedUbrukMarkDef]);
+    const filtreringsValg = getAlleTilpassInnholdValg(parsedText);
+
+    expect(filtreringsValg).toHaveLength(0);
+  });
 });
--- src/components/faktaside/TilpassInnhold/getAlleTilpassInnholdValg.ts
+++ src/components/faktaside/TilpassInnhold/getAlleTilpassInnholdValg.ts
@@ -1,18 +1,28 @@
 import getPropertyRecursivlyFromDeepObject from "../../../utils/getPropertyRecursivlyFromDeepObject";
-import { Block, MarkDef, SanityBlock } from "../../../utils/richTextUtils/richTextTypes";
+import { Block, BlockConfigFromParser, MarkDef, SanityBlock } from "../../../utils/richTextUtils/richTextTypes";
 import { visForAnnotationTypes } from "../../../utils/richTextUtils/allChildrenMarkedWith";
 import { getUniqueStrings } from "../../BlockContent/VisFor/VisFor";
 
 export type TilpassInnholdValg = string[];
 
+function fjernMarkDefsSomIkkeErIBruk(markDefs: MarkDef[], altInnhold: Block[]) {
+  const alleMarks = getPropertyRecursivlyFromDeepObject<string>(altInnhold, "marks");
+  return markDefs.filter((markDef) => alleMarks.includes(markDef._key));
+}
+
 function getAlleTilpassInnholdValg(innhold?: Block[], kortFortalt?: SanityBlock[]): TilpassInnholdValg {
   if (!innhold) return [];
   const altInnhold = [...innhold, ...(kortFortalt || [])];
+
   const alleMarkDefs = getPropertyRecursivlyFromDeepObject<MarkDef>(altInnhold, "markDefs");
   const alleVisForMarkDefs = alleMarkDefs.filter((markDef) => visForAnnotationTypes.includes(markDef._type));
-  const samletVisForConfig = alleVisForMarkDefs.flatMap((it) => it.visFor?.situasjoner || []);
+  const visForMarkDefsSomErIBruk = fjernMarkDefsSomIkkeErIBruk(alleVisForMarkDefs, altInnhold);
+  const situasjonerFraMarkDefs = visForMarkDefsSomErIBruk.flatMap((it) => it.visFor?.situasjoner || []);
 
-  return getUniqueStrings(samletVisForConfig);
+  const alleBlockConfigs = getPropertyRecursivlyFromDeepObject<BlockConfigFromParser>(altInnhold, "blockConfig");
+  const situasjonerFraBlockConfig = alleBlockConfigs.flatMap((it) => it.visFor?.situasjoner || []);
+
+  return getUniqueStrings([...situasjonerFraMarkDefs, ...situasjonerFraBlockConfig]).sort();
 }
 
 export default getAlleTilpassInnholdValg;
