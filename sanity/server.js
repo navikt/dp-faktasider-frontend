c57aacbf465aa3f1cb1be38ef6534b9f7164fb7f
Daniel Winsvold <danielwinsvold@gmail.com>
2021-03-11 10:17:59 +0100

    forenkler litt

--- sanity/server.js
+++ sanity/server.js
@@ -5,21 +5,17 @@ const app = express();
 
 const buildPath = path.resolve(__dirname, "./dist");
 const basePath = "/arbeid/cms";
-app.set("views", buildPath);
-
-// Parse application/json
-app.use(express.json());
-
-// Static files
-app.use(basePath, express.static(buildPath, { index: false }));
 
 // Nais functions
 app.get(`${basePath}/internal/isAlive|isReady`, (req, res) => res.sendStatus(200));
 
+// Static files
+app.use(basePath, express.static(buildPath, { index: false }));
+
 // Match everything except internal og static
 app.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) => res.sendFile(buildPath + "/index.html"));
 
-const port = process.env.PORT || 8000;
+const port = 8000;
 const server = app.listen(port, () => console.info(`🚀 App listening on port: ${port}`));
 
 process.on("SIGINT", () => {
