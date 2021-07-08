9614a5a407d01fbb8d548c92e654bdff06de8d07
winsvold <29202094+winsvold@users.noreply.github.com>
2021-07-08 13:06:06 +0200

    fikser brødsmuleurl så den gir riktig url i koden (hånderes i javascript, så har funka selv om url'en var feil)

--- src/components/faktaside/useBreadcrumbs.tsx
+++ src/components/faktaside/useBreadcrumbs.tsx
@@ -3,11 +3,11 @@ import { useRouter } from "next/router";
 import { Breadcrumb, onBreadcrumbClick, setBreadcrumbs } from "@navikt/nav-dekoratoren-moduler";
 
 function useBreadcrumbs(forsideTittel?: string, sider?: { tittel: string; path: string }[]) {
-  const navigate = useRouter().push;
-  const forsideUrl = `/`;
+  const { push: navigate, basePath } = useRouter();
+  const forsideUrl = `${basePath}/`;
 
   onBreadcrumbClick((breadcrumb) => {
-    navigate(breadcrumb.url);
+    navigate(breadcrumb.url.replace(`${basePath}`, ""));
   });
 
   const breadcrumbs: Breadcrumb[] = [{ title: forsideTittel || "Forside", url: forsideUrl, handleInApp: true }];
