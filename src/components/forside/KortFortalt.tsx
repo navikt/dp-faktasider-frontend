59645979c9e4fe149a164f2651256a001410cefd
winsvold <29202094+winsvold@users.noreply.github.com>
2021-07-08 10:05:03 +0200

    tweaker på styling forside

--- src/components/forside/KortFortalt.tsx
+++ src/components/forside/KortFortalt.tsx
@@ -9,13 +9,16 @@ import { useTranslation } from "react-i18next";
 
 const Style = styled.article`
   max-width: ${contentMaxwidth};
-  ${typografiStyle};
   padding: 2rem;
   background-color: white;
 `;
 
 const Content = styled.div`
   max-width: 40rem;
+  ${typografiStyle};
+  li {
+    margin: 0.2rem 0 !important; // Litt mindre luft mellom bulletpoints på forside for å spare plass så brukerne ser navigasjon til videre sider i første skjermbilde
+  }
 `;
 
 function KortFortalt(props: { kortFortalt?: ParsedRichText; beskrivelse?: string }) {
