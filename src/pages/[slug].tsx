cce65c9fd12415ccdcff6148a39d99335787d397
winsvold <29202094+winsvold@users.noreply.github.com>
2021-07-14 14:02:04 +0200

    fjerner avhengighet til spinner

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -8,9 +8,11 @@ import { parseFaktasideData } from "../sanity/groq/faktaside/parseFaktasideData"
 import { useLocale } from "../i18n/useLocale";
 import { menuQuery, MenuQueryData } from "../sanity/groq/menu/menuQuery";
 import { parseMenuData } from "../sanity/groq/menu/parseMenuData";
-import NavFrontendSpinner from "nav-frontend-spinner";
 import { supportedLanguages } from "../i18n/supportedLanguages";
 import { useSanityPreveiw } from "../sanity/useSanityPreview";
+import React from "react";
+import { Alert } from "@navikt/ds-react";
+import { loggError } from "../utils/logging";
 
 const pathsQuery = groq`*[_type == "faktaSide"][].slug.current`;
 
@@ -60,7 +62,8 @@ function PreviewWrapper(props: Props) {
   const locale = useLocale();
 
   if (!faktasideData?.faktaside?.id) {
-    return <NavFrontendSpinner />;
+    loggError(new Error("Fant ikke faktaside"));
+    return <Alert variant="error">Fant ikke siden du leter etter</Alert>;
   }
 
   const parsedFaktasideData = parseFaktasideData(faktasideData, locale);
