14ccf19545f1f6278435afee12e487611ec66848
Sindre Sægrov <sindre.segrov@nav.no>
2021-10-28 15:28:16 +0200

    Breadcrumb type is not exported from nav-dekoratoren

--- src/components/faktaside/useBreadcrumbs.tsx
+++ src/components/faktaside/useBreadcrumbs.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { Breadcrumb, onBreadcrumbClick, setBreadcrumbs } from "@navikt/nav-dekoratoren-moduler";
+import { onBreadcrumbClick, setBreadcrumbs } from "@navikt/nav-dekoratoren-moduler";
 
 function useBreadcrumbs(forsideTittel?: string, sider?: { tittel: string; path: string }[]) {
   const { push: navigate, basePath } = useRouter();
@@ -10,7 +10,7 @@ function useBreadcrumbs(forsideTittel?: string, sider?: { tittel: string; path: 
     navigate(breadcrumb.url.replace(`${basePath}`, ""));
   });
 
-  const breadcrumbs: Breadcrumb[] = [{ title: forsideTittel || "Forside", url: forsideUrl, handleInApp: true }];
+  const breadcrumbs = [{ title: forsideTittel || "Forside", url: forsideUrl, handleInApp: true }];
 
   sider?.forEach((side) => {
     const sideUrl = `${forsideUrl}${side.path}`;
