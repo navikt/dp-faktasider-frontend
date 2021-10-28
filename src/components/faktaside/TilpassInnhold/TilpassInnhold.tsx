4b31aeff5bd5c429784930303e60b8cb9a2b7276
Sindre Sægrov <sindre.segrov@nav.no>
2021-10-28 14:54:45 +0200

    Revert section to nav

--- src/components/faktaside/TilpassInnhold/TilpassInnhold.tsx
+++ src/components/faktaside/TilpassInnhold/TilpassInnhold.tsx
@@ -3,7 +3,6 @@ import { useVisForContext } from "../../BlockContent/VisFor/VisForContext";
 import styled from "styled-components/macro";
 import { withErrorBoundary } from "../../withErrorBoundary";
 import { theme } from "../../../styles/theme";
-import useUniqueId from "../../../utils/useUniqueId";
 import { UnmountClosed } from "react-collapse";
 import { useFaktasideContext } from "../FaktaSideContext";
 import { useWordCount } from "./useWordCount";
@@ -13,7 +12,7 @@ import { BodyShort, Title } from "@navikt/ds-react";
 import Checkbox from "../../Checkbox";
 import { TestId } from "../../../utils/test-ids";
 
-const StyledNav = styled.section`
+const StyledNav = styled.nav`
   background-color: white;
   padding: ${theme.layoutPadding} calc(${theme.layoutPadding} * 1.75);
   margin-bottom: ${theme.layoutMargin};
